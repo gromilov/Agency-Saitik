678c92ee2d7e175180e545ffdc9b0d1bfa63d693
gromilov <max.gromilov@yandex.ru>
2026-03-07 10:28:19 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '/var/www/sites/gromilov.saitik.su',
+  devToolbar: { enabled: false },
+
+  server: {
+    host: '0.0.0.0',
+    port: 4322
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
